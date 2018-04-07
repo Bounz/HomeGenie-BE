d103581a0da150d0559e6431d7b8d424c4f3677f
Alexander Sidorenko <me@bounz.net>
2018-04-07 20:32:36 +0300

    Fixes to installer script

--- Utils/HgBootstrapper/installer.sh
+++ Utils/HgBootstrapper/installer.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+LG='\033[1;32m' # Light green
+YL='\033[1;33m' # Yellow
+RED='\033[0;31m' # Red
+NC='\033[0m'     # No Color
 default_install_directory=/usr/local/bin/hgbe
 
 #Docker
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker is already installed"
+    echo "${LG}Docker is already installed${NC}"
 else
-    echo "Install docker"
+    echo "${YL}Install docker${NC}"
     #curl -sSL https://get.docker.com | sh
     wget -qO- https://get.docker.com | sh
-    echo "Docker intallation complete"
+    echo "${LG}Docker intallation complete${NC}\n"
 fi
 
 #HomeGenie
-read -p "Enter installation path [${default_install_directory}]: " install_directory
+echo "Enter installation path [${default_install_directory}]: "
+read install_directory
 install_directory=${install_directory:-${default_install_directory}}
 echo "Installing HomeGenie (Bounz Edition) into $install_directory"
 
@@ -23,11 +28,16 @@ downloadSource=https://raw.githubusercontent.com/Bounz/HomeGenie-BE/master/Utils
 #curl -o $install_directory/service/start.sh ${downloadSource}/start.sh
 #curl -o $install_directory/service/stop.sh ${downloadSource}/stop.sh
 #curl -o /etc/systemd/system/hgbe.service ${downloadSource}/hgbe.svc
-wget -O $install_directory/service/start.sh ${downloadSource}/start.sh
-wget -O $install_directory/service/stop.sh ${downloadSource}/stop.sh
-wget -O /etc/systemd/system/hgbe.service ${downloadSource}/hgbe.svc
+wget -qO $install_directory/service/start.sh ${downloadSource}/start.sh
+wget -qO $install_directory/service/stop.sh ${downloadSource}/stop.sh
+wget -qO /etc/systemd/system/hgbe.service ${downloadSource}/hgbe.svc
 
-sed -i '' "s#__install_directory__#${install_directory}#g" /etc/systemd/system/hgbe.service
+sed -i "s#__install_directory__#${install_directory}#g" /etc/systemd/system/hgbe.service
 
 systemctl enable hgbe
 systemctl start hgbe
+
+echo "${LG}HomeGenie (Bounz Edition) successfully installed${NC}"
+echo "Use following commands to start/stop service:"
+echo "${YL}sudo systemctl start hgbe${NC}"
+echo "${YL}sudo systemctl stop hgbe${NC}"
