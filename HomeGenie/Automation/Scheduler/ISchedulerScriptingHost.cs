d665b872ff443f0366a086a9190a9e7559fd87c1
Alexander Sidorenko <me@bounz.net>
2018-04-11 08:42:46 +0300

    HGBE #41. Fix scheduler wrapper script, updated Jint.

---
+++ HomeGenie/Automation/Scheduler/ISchedulerScriptingHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HomeGenie.Automation.Scripting;
+using HomeGenie.Data;
+using HomeGenie.Service;
+
+namespace HomeGenie.Automation.Scheduler
+{
+    public interface ISchedulerScriptingHost
+    {
+        void SetHost(HomeGenieService hg, SchedulerItem item);
+        void RouteModuleEvent(ProgramManager.RoutedEvent eventData);
+        SchedulerScriptingHost OnModuleUpdate(Action<ModuleHelper, ModuleParameter> handler);
+        ProgramHelperBase Program { get; }
+        ModulesManager Modules { get; }
+        ModulesManager BoundModules { get; }
+        SettingsHelper Settings { get; }
+        NetHelper Net { get; }
+        SerialPortHelper SerialPort { get; }
+        TcpClientHelper TcpClient { get; }
+        UdpClientHelper UdpClient { get; }
+        MqttClientHelper MqttClient { get; }
+        KnxClientHelper KnxClient { get; }
+        SchedulerHelper Scheduler { get; }
+        ModuleParameter Data(string name);
+        void Pause(double seconds);
+        void Delay(double seconds);
+        void Say(string sentence, string locale = null, bool goAsync = false);
+        void Reset();
+    }
+}
---
+++ HomeGenieTests/SchedulerScriptingEngineTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using HomeGenie.Automation.Scheduler;
+using HomeGenie.Service;
+using Jint;
+using Moq;
+using NUnit.Framework;
+
+namespace HomeGenieTests
+{
+    [TestFixture]
+    public class SchedulerScriptingEngineTests
+    {
+        [Test]
+        public void Program_WihoutUseOfScriptingHost_Works()
+        {
+            var jintEngine = new Engine();
+            jintEngine.SetValue("hg", new Object());
+            jintEngine.Execute(SchedulerScriptingEngine.InitScript);
+        }
+
+        [Test]
+        public void Program_Say_Works()
+        {
+            var param = "";
+            var scriptingHost = new Mock<ISchedulerScriptingHost>();
+            scriptingHost
+                .Setup(x => x.Say(It.IsAny<string>(), null, false))
+                .Callback((string x, string s, bool b) => param = x);
+
+            var jintEngine = new Engine();
+            jintEngine.SetValue("hg", scriptingHost.Object);
+
+            jintEngine.Execute(SchedulerScriptingEngine.InitScript + "hg.Say('test', null, false);");
+
+            Assert.That(param, Is.EqualTo("test"));
+        }
+    }
+}
