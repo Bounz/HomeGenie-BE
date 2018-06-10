2af0585da0eb59135374a9fc583bfd5123eb5ae3
Alexander Sidorenko <me@bounz.net>
2018-06-10 23:00:39 +0300

    HGBE #10. Commiting lost files.

---
+++ HomeGenie/Service/Packages/PackageInterfaceDefinition.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace HomeGenie.Service.Packages
+{
+    public class PackageInterfaceDefinition
+    {
+        [JsonProperty("file")]
+        public string File { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ HomeGenie/Service/Packages/PackageProgramDefinition.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace HomeGenie.Service.Packages
+{
+    public class PackageProgramDefinition
+    {
+        [JsonProperty("file")]
+        public string File { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("uid")]
+        public string Uid { get; set; }
+
+        [JsonProperty("group")]
+        public string Group { get; set; }
+    }
+}
