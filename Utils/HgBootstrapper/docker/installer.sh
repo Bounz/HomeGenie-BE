e910d5d5f0202d9d0a33e10be44d5a3df17b7600
Alexander Sidorenko <me@bounz.net>
2018-09-02 20:55:35 +0300

    Using the same data and logs directories as non-docker installation by default.

--- Utils/HgBootstrapper/docker/installer.sh
+++ Utils/HgBootstrapper/docker/installer.sh
@@ -58,6 +58,7 @@ $sh_c "wget -qO $install_directory/service/stop.sh ${downloadSource}/stop.sh"
 $sh_c "wget -qO /etc/systemd/system/hgbe.service ${downloadSource}/hgbe.svc"
 
 $sh_c "sed -i \"s#__install_directory__#${install_directory}#g\" /etc/systemd/system/hgbe.service"
+$sh_c "sed -i \"s#__install_directory__#${install_directory}#g\" $install_directory/service/start.sh"
 
 $sh_c "systemctl enable hgbe"
 $sh_c "systemctl start hgbe"
